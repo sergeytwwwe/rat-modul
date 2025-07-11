e4b3dd5a92ad0c7bed2c16760663b38c7d4e3d00
sergeytwwwe <153748259+sergeytwwwe@users.noreply.github.com>
2025-07-11 20:05:37 +0400

    Update terminate_process.py

--- terminate_process.py
+++ terminate_process.py
@@ -7,9 +7,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 def terminate_process_by_name(process_name: str) -> str:
-    """Завершает процесс по имени"""
+    """Завершает процесс по имени, добавляя .exe, если указано без расширения"""
     try:
         logger.info(f"💀 Получена команда: Завершить процесс {process_name}")
+        # Если имя без расширения, добавляем .exe
+        if not process_name.lower().endswith('.exe'):
+            process_name = f"{process_name}.exe"
+        
         for proc in psutil.process_iter(['name']):
             if proc.info['name'].lower() == process_name.lower():
                 proc.terminate()
